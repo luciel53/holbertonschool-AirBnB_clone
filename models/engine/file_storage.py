7c4377110688bc1475e350d5539eb4b81fd9612d
crasride <jose.dfernandez@yahoo.fr>
2022-10-11 17:36:24 +0200

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -15,25 +15,25 @@ class FileStorage():
 
     def new(self, obj):
         """ sets in __objects the obj with key <obj class name>.id """
-        # self.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
-        key = obj.__class__.__name__ + '.' + obj.id
-        self.__objects[key] = obj
+        self.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
+        # key = obj.__class__.__name__ + '.' + obj.id
+        # self.__objects[key] = obj
 
     def save(self):
         """ serializes __objects to the JSON file """
-        with open(self.__file_path, 'w') as f:
-            jdict = {}
-            for name, obj in self.__objects.items():
-                jdict[name] = obj.to_dict()
-            json.dump(jdict, f)
+        with open(self.__file_path, mode='w', encoding='utf-8') as f:
+            obj_dict = {key_obj: obj.to_dict()
+                        for key_obj, obj in self.__objects.items()}
+            json.dump(obj_dict, f)
         
 
     def reload(self):
         """ Deserializes JSON to __objects (only if JSON file (__file_path) """
         try:
             with open(self.__file_path, mode='r', encoding='utf-8') as f:
-                for key, value in (json.load(f)).items():
-                    value = eval(value["__class__"])(**value)
-                    self.__objects[key] = value
-        except Exception:
+                all_objs = json.loads(f.read())
+            for obj_id, obj in all_objs.items():
+                name_class = obj_id.split(".")[0]
+                self.new(eval(name_class + "(**obj)"))
+        except:
             pass
