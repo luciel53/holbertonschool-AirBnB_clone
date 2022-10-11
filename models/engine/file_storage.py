b287f465defe06c11979db7ba34a60f6497bf6c2
crasride <jose.dfernandez@yahoo.fr>
2022-10-11 17:49:37 +0200

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -15,25 +15,29 @@ class FileStorage():
 
     def new(self, obj):
         """ sets in __objects the obj with key <obj class name>.id """
-        self.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
-        # key = obj.__class__.__name__ + '.' + obj.id
-        # self.__objects[key] = obj
+        key = type(obj).__name__ + "." + obj.id
+        FileStorage.__objects[key] = obj
+        # self.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
 
     def save(self):
         """ serializes __objects to the JSON file """
-        with open(self.__file_path, mode='w', encoding='utf-8') as f:
-            obj_dict = {key_obj: obj.to_dict()
-                        for key_obj, obj in self.__objects.items()}
-            json.dump(obj_dict, f)
+        new_dict = {}
+        for key, value in self.__objects.items():
+            new_dict[key] = value.to_dict()
+
+        with open(self.__file_path, mode='w', encoding='utf-8') as file:
+            json.dump(new_dict, file)
         
 
     def reload(self):
         """ Deserializes JSON to __objects (only if JSON file (__file_path) """
         try:
-            with open(self.__file_path, mode='r', encoding='utf-8') as f:
-                all_objs = json.loads(f.read())
-            for obj_id, obj in all_objs.items():
-                name_class = obj_id.split(".")[0]
-                self.new(eval(name_class + "(**obj)"))
-        except:
-            pass
+            with open(self.__file_path, "r") as file:
+                obj_dict = json.load(file)
+
+                for key, value in obj_dict.items():
+                    name_class = eval(value["__class__"])(**value)
+                    self.__objects[key] = name_class
+
+        except FileNotFoundError:
+            return
