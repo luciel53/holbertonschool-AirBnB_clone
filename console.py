dba6324b3a664caca0c4f97f5830671a10b9d33e
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 <l.leroty@gmail.com>
2022-10-12 22:00:08 +0200

    add the console to complete task 10

--- console.py
+++ console.py
@@ -7,6 +7,11 @@ from models.base_model import BaseModel
 from models import storage
 import cmd
 from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class HBNBCommand(cmd.Cmd):
@@ -15,7 +20,9 @@ class HBNBCommand(cmd.Cmd):
     """
     prompt = "(hbnb) "
 
-    list_classes = {"BaseModel": BaseModel, "User": User}
+    list_classes = {"BaseModel": BaseModel, "User": User, "State": State,
+                    "City": City, "Amenity": Amenity, "Place": Place,
+                    "Review": Review}
 
     def do_quit(self, args):
         """
