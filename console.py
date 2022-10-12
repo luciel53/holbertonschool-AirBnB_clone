cd2d6f39056eecc80b05567a875b8de15f740380
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 <l.leroty@gmail.com>
2022-10-12 11:06:08 +0200

    console create

--- console.py
+++ console.py
@@ -4,6 +4,7 @@ Write a program called console.py that contains the entry point of
 the command interpreter
 """
 from models.base_model import BaseModel
+from models.engine.file_storage import storage
 import cmd
 
 
@@ -30,6 +31,20 @@ class HBNBCommand(cmd.Cmd):
         Empty the line of the command interpreter
         """
         return False
+
+    def do_create(self, args):
+        """
+        Creates a new instance of BaseModel, saves it (to the JSON file)
+        and prints the id
+        """
+        if args == "":
+            print("** class name missing **")
+        elif args not in storage.classes():
+            print("** class doesn't exist **")
+
+        else:
+            model = storage.create_model(args)
+            print(model.id)
 
 
 if __name__ == '__main__':
