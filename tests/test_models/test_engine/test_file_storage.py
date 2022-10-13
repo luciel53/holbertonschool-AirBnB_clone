b4ac5af598b95ed69d7459ca492fa82300eb6672
crasride <jose.dfernandez@yahoo.fr>
2022-10-13 15:18:24 +0200

    file sorage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -2,8 +2,6 @@
 "Unit tests for FileStorage class"
 import unittest
 from models.engine.file_storage import FileStorage
-from models import storage
-from models.base_model import BaseModel
 
 
 class TestFileStorage(unittest.TestCase):
@@ -13,6 +11,16 @@ class TestFileStorage(unittest.TestCase):
         "Tests that FileStorage correctly instanciates"
         storage = FileStorage()
         self.assertIsInstance(storage, FileStorage)
+
+    def test_file_path(self):
+        """Test __file type is correct after deserialization string-str"""
+        file_path_str = FileStorage._FileStorage__file_path
+        self.assertEqual(str, type(file_path_str))
+
+    def test_object(self):
+        """Test __object is type dict after deserialization object - dict"""
+        object_dict = FileStorage._FileStorage__objects
+        self.assertEqual(dict, type(object_dict))
 
 
 if __name__ == "__main__":
