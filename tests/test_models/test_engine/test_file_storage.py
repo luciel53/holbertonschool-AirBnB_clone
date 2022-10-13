aebb6be09bfd4af2a4e5a17ab36e9692830aee27
crasride <jose.dfernandez@yahoo.fr>
2022-10-13 14:15:39 +0200

    test all

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -2,6 +2,11 @@
 "Unit tests for FileStorage class"
 import unittest
 from models.engine.file_storage import FileStorage
+from models import storage
+from datetime import datetime
+import time
+import json
+import os
 
 
 class TestFileStorage(unittest.TestCase):
@@ -11,6 +16,17 @@ class TestFileStorage(unittest.TestCase):
         "Tests that FileStorage correctly instanciates"
         storage = FileStorage()
         self.assertIsInstance(storage, FileStorage)
+
+    def Help_test_all(self, classname):
+        """ tests for classname."""
+        self.resetStorage()
+        self.assertEqual(storage.all(), {})
+
+        new = storage.classes()[classname]()
+        storage.new(o)
+        key = "{}.{}".format(type(new).__name__, new.id)
+        self.assertTrue(key in storage.all())
+        self.assertEqual(storage.all()[key], new)
 
 
 if __name__ == "__main__":
